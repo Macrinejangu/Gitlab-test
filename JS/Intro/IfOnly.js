1396cbaaca09d5442ea47b643114caa207f03f81
Macrine <macrinejangu@gmail.com>
2026-06-24 08:55:30 +0100

    explored the condition if

---
+++ JS/Intro/IfOnly.js
@@ -0,0 +1,29 @@
+/*
+Condition if
+- it's special
+- You can use it on it's own
+*/
+
+
+let age=prompt("Insert your age")
+alert(`You added ${age}`)
+
+if(age<10){
+    alert(`Its Time to Play!`)
+}
+
+if(age>13){
+    alert(`How is teenage life so far?`)
+}
+
+if(age>18){
+    alert(`Welcome to adulthood!`)
+}
+
+if(age>25){
+    alert(`I hope you're living your best life`)
+}
+
+if(age>35){
+    alert(`You are exactly what you think you are!`)
+}
