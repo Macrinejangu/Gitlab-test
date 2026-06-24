7c3e25ac8584c28e96ccfba5f0e34d4f32ea2d94
Macrine <macrinejangu@gmail.com>
2026-06-24 12:47:55 +0100

    learned about if and else if

---
+++ JS/Intro/IfElseIf.js
@@ -0,0 +1,33 @@
+/*
+Condition if
+- it's special
+- You can use it on it's own
+*/
+
+
+let age=prompt("Insert your age")
+alert(`You added ${age}`)
+
+if(age<10){
+    alert(`Its Time to Play!`)
+}
+
+else if(age>13){
+    alert(`How is teenage life so far?`)
+}
+
+else if(age>18){
+    alert(`Welcome to adulthood!`)
+}
+
+else if(age>25){
+    alert(`I hope you're living your best life`)
+}
+
+else if(age>35){
+    alert(`You are exactly what you think you are!`)
+}
+
+/* 
+If and else if, we exit with the first truth
+*/
