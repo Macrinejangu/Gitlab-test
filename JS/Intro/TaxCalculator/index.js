3dc464da51214bc0bd90536a9097932e5e9a2b43
Macrine <macrinejangu@gmail.com>
2026-06-26 09:06:49 +0100

    Completed the tax Calculator exercise

--- JS/Intro/TaxCalculator/index.js
+++ JS/Intro/TaxCalculator/index.js
@@ -26,33 +26,84 @@ console.log(`Your Gross Salary is ${monthlyGrossSalary} is type of ${typeof mont
 
 let paye=null;
 let tier=null;
+
 //Tier 1
-if (grossMonthlySalary >= 0 && grossMonthlySalary <= 24000) {
-    paye = grossMonthlySalary * 0.10
-    tier = "0-24,000 Ksh"
-}
-//Tier 2
-else if (grossMonthlySalary <= 32333) {
-    paye = grossMonthlySalary * 0.25
-     tier ="24,000-32,333 Ksh"
+if (monthlyGrossSalary <= 24000) {
+    tax = monthlyGrossSalary * 0.10
+    tier = "0-24000 Ksh"
 } 
-
+// Tier 2
+else if (monthlyGrossSalary <= 24000 + 8333) {
+    paye = (24000 * 0.10) + ((salary - 24000) * 0.25)
+    tier = "24001-32333 Ksh"
+} 
 //Tier 3
-else if (grossMonthlySalary <= 500000) {
-    paye = grossMonthlySalary * 0.30
-     tier="32,333-500,000 Ksh"
+else if (monthlyGrossSalary <= 24000 + 8333 + 467667) {
+    paye = (24000 * 0.10) + (8333 * 0.25) + ((monthlyGrossSalary - 24000 - 8333) * 0.30)
+    tier = "32333-500000 Ksh"
 } 
-
-//Tier 4
-else if (grossMonthlySalary <= 800000) {
-    paye = grossMonthlySalary * 0.325
-     tier="500,000-800,000 Ksh"
+// Tier 4
+else if (monthlyGrossSalary <= 24000 + 8333 + 467667 + 300000) {
+    paye = (24000 * 0.10) + (8333 * 0.25) + (467667 * 0.30) + ((salary - 24000 - 8333 - 467667) * 0.325)
+     tier = "500000-Infinity Ksh"
 } 
-
 // Tier 5
 else {
-    paye = grossMonthlySalary * 0.35
-     tier="800,000-Infinity Ksh"
+    paye = (24000 * 0.10) + (8333 * 0.25) + (467667 * 0.30) + (300000 * 0.325) + ((salary - 24000 - 8333 - 467667 - 300000) * 0.35)
+    tier = "800000-800000 Ksh"
 }
 
-console.log(`PAYE is ${paye}`)
+alert(`PAYE is ${paye} and your in tier ${tier}`)
+
+//NSSF
+let nssf=0
+if (monthlyGrossSalary<=9000){
+    nssf=monthlyGrossSalary * 0.06;
+}
+else if(monthlyGrossSalary<=108000){
+    nssf = (9000*0.06) + ((monthlyGrossSalary - 9000) *0.06);
+}
+else{
+    nssf = 6840
+}
+
+let employerNssf = nssf;
+let totalNssf = nssf + employerNssf
+
+alert(`employer NSSF is ${nssf}, Employer NSSF is ${employerNssf}, Total NSSF is ${totalNssf}`)
+
+//Personal Relief
+let personalRelief = 2400 //This is a fixed monthly deducted amount.
+let netPaye = paye - personalRelief;
+if (netPaye<0){
+    netPaye=0;
+}
+
+alert(`Your Gross PAYE before personal relief = ${paye}
+Personal relief = ${personalRelief} 
+Net PAYE payable = ${netPaye}`)
+
+//SHIF
+let shif = monthlyGrossSalary * 0.0275
+if (shif<300){
+    shif=300;
+}
+
+alert(`SHIF = ${shif}`)
+
+//Affordable Housing
+let housingLevy = monthlyGrossSalary * 0.015;
+
+alert(`Housing Levy = ${housingLevy}`)
+
+//NET PAY/ Taxable Income
+
+let netPay = monthlyGrossSalary - netPaye - nssf - shif - housingLevy
+
+alert(`Gross Salary = ${monthlyGrossSalary}
+    NSSF = ${nssf}
+    SHIF = ${shif}
+    Housing Levy = ${housingLevy}
+    Personal Relief = ${personalRelief}
+    Net Paye = ${netPaye}
+    Net Paye = ${netPay}`)
