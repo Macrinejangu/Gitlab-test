b511473f67a91592cd8c6cf6c227eb7d2e765411
Macrine <macrinejangu@gmail.com>
2026-08-25 10:21:27 +0300

    began intro to python

---
+++ PYTHON/Intro/data_type.py
@@ -0,0 +1,17 @@
+a= 10       #Integer
+b= 3.14    #Float
+c= "Hello"  #String
+d= True      #Boolean <True, False>
+e= [1,2,3,4] #List, Array
+f= {1,2,3} #Set
+g=(1,2,3,4) #Tuple (An Immutable List)
+h={"key", "value"}
+
+print ("A is", a, "Its type is", type(a))
+print(f"B is {b} its type is {type(b)}")
+print ("C is", c, "Its type is", type(c))
+print(f"D is {d} its type is {type(d)}")
+print ("E is", e, "Its type is", type(e))
+print(f"F is {f} its type is {type(f)}")
+print ("G is", g, "Its type is", type(g))
+print(f"H is {h} its type is {type(h)}")
---
+++ PYTHON/Intro/start.py
@@ -0,0 +1,10 @@
+# Here comments are made using #
+# We don't use console.log, we use print
+#
+
+x=34
+print("X is =", x)
+y=20
+print("Y is =", y)
+z=x+y
+print("X+Y is", z)
