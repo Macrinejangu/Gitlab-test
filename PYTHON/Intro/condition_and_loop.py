52a6398b97aaec5ebe524caa60cad52cee076f51
Macrine <macrinejangu@gmail.com>
2026-08-26 10:34:53 +0300

    Made changes to my comments so they make more sense

--- PYTHON/Intro/condition_and_loop.py
+++ PYTHON/Intro/condition_and_loop.py
@@ -1,46 +1,122 @@
-# Python uses indentation (spaces at the beginning of a line)
-# to show which lines belong to a block of code.
+# 1. INDENTATION
 
-#Example
+
+# Python uses indentation to show which lines
+# belong to a block of code.
+#
+# In JavaScript, curly braces {} usually define
+# the beginning and end of a block.
+#
+# Python:
+#
+# if age > 18:
+#     print("You are over 18")
+#
+# JavaScript:
+#
+# if (age > 18) {
+#     console.log("You are over 18");
+# }
+
+
+# 2. IF, ELIF, ELSE
+
+
 age = 20
+
+# 'if' checks whether a condition is True.
+if age > 18:
+    print("You are over 18")
+
+# 'elif' means "else if".
+# Python checks this only if the first 'if' was False.
+elif age == 18:
+    print("You are exactly 18")
+
+# 'else' runs when none of the conditions above are True.
+else:
+    print("You are under 18")
+
+
+
+# 3. NESTED IF
+
+
+age = 23
 
 if age > 18:
     print("You are over 18")
-    print("This also belongs to the if statement")
-#The two print() statements are indented, so Python knows both belong to the if.
 
-#If Conditions
-age=20
-if age>18:
-    print("You can Drink")
-    print("You can't smoke")
-    if age== 23:
-        print("You are youth")
-    print("You are still a teenager") #This belongs in the first 'if'
+    # This if is inside the first if.
+    if age == 23:
+        print("You are exactly 23")
 
-#else if condition 
-# 
-elif age==44:
-    print("Such a young age")
-else:
-    print("Last else")
+    # This belongs to the first if.
+    print("Your age is above 18")
 
-k=0
 
-while k<30:
-    k=k+1
-    print("kis", k)
 
-#When writing Range(Start, Stop, Step)
-for i in range(0,5,1):
-    print("I is", i)
+# 4. RANGE
 
-ar=[23,"Hello", 76,45,40]
-#in JS, we'd write for(let i=0;i<ar.length;i++){0-99}
-for i in range(0,len(ar)):
-    single_item=ar[i]
+
+# range(start, stop, step)
+#
+# start = where we begin
+# stop = where we stop, excluding this number
+# step = how much we increase by
+
+for i in range(0, 5, 1):
+    print(i)
+
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
+
+
+
+# 5. PYTHON LISTS
+
+
+ar = [23, "Hello", 76, 45, 40]
+
+# Lists use indexes starting at 0.
+#
+# Index:  0       1        2       3       4
+#         ↓       ↓        ↓       ↓       ↓
+# ar = [23,    "Hello",   76,     45,     40]
+
+
+
+# 6. LOOPING USING AN INDEX
+
+
+# len(ar) tells us how many items are in the list.
+# len(ar) is 5 in this example.
+
+for i in range(0, len(ar)):
+    single_item = ar[i]
     print(single_item)
 
-#for(let singleItem of array)
+# This is similar to JavaScript:
+#
+# for (let i = 0; i < ar.length; i++) {
+#     console.log(ar[i]);
+# }
+
+
+# 7. LOOPING DIRECTLY THROUGH THE ITEMS
+
+# Python lets us loop directly through the list.
+# We don't need to create an index ourselves.
+
 for single_item in ar:
-    print("Single item", single_item)            
+    print("Single item:", single_item)
+
+# This is similar to JavaScript:
+#
+# for (let singleItem of ar) {
+#     console.log("Single item:", singleItem);
+# }
--- PYTHON/Intro/function.py
+++ PYTHON/Intro/function.py
@@ -1,15 +1,43 @@
-#function areaCircle(radius)
-#{return 3.142*radius*radius}
-
-#In Python, we use def
-
+# FUNCTIONS
+#
+# A function is a reusable block of code.
+#
+# JavaScript:
+#
+# function areaCircle(radius) {
+#     return 3.142 * radius ** 2;
+# }
+#
+# Python uses 'def' instead of 'function'.
+#
+# Python:
+#
 def area_circle(radius):
-    ans= 3.142*pow(radius,2)
-    print(f"for cicle of radius {radius} area is {ans}")
-    return
+    # Calculate the area of the circle.
+    # ** 2 means "squared".
+    ans = 3.142 * radius ** 2
 
+    # f-strings allow us to put variables inside text.
+    print(f"For a circle with radius {radius}, the area is {ans}")
+
+    # return sends the calculated answer back to whoever
+    # called the function.
+    return ans
+
+
+# CALLING A FUNCTION
+#
+# Give the function 20 as the radius.
 area_circle(20)
 
-new_f=area_circle
-new_f(10)
 
+# FUNCTIONS CAN BE STORED IN VARIABLES
+#
+# This stores a reference to the function.
+# It does not run the function yet.
+new_f = area_circle
+
+
+# Because new_f refers to area_circle,
+# we can use new_f to call the function.
+new_f(10)
